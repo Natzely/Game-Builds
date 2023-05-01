2070ebca2b5297db15ada625c1dca55c14bb45d1
Edgar <edgalvazquez@gmail.com>
2023-04-30 22:43:50 -0500

    Update for 0.2.6

--- Butler Update.bat
+++ Butler Update.bat
@@ -1,2 +1,2 @@
-butler push OGL_OcachiOmpa.zip teotlgames/ocachiompa:html5 --userversion 0.2.4.4
-butler push WIN_OcachiOmpa.zip teotlgames/ocachiompa:win64 --userversion 0.2.4.4
+butler push OGL_OcachiOmpa.zip teotlgames/ocachiompa:html5 --userversion 0.2.6
+butler push WIN_OcachiOmpa.zip teotlgames/ocachiompa:win64 --userversion 0.2.6
